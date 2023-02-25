0025f4edba637591e4e3b2bd29301be0a724adb0
Jack <li.jack0707@gmail.com>
2023-02-25 13:02:17 -0800

    chore: added new lading page sections, reorganized pages, added new data file

---
+++ pages/Home-data.jsx
@@ -0,0 +1,25 @@
+
+const Homedata = [
+  {
+    title: "#1 Quality Noodles",
+    description:
+      "All noodles are hand-made everyday by our friendly chefs. See it yourself through the uchiba window and wave hello! 👋",
+    url: "/images/home/specialty-1.png",
+  },
+  {
+    title: "#2 Foreigner Friendly",
+    description:
+      "Our staffs are from many corners of the world. The owner, Mr.Ogawa, is known for his friendliness to locals & foreigners alike 😄",
+    url: "/images/home/specialty-2.png",
+  },
+  {
+    title: "#B...Black Udon!?",
+    description:
+      "A unique blend of udon using bamboo charcoal. It's a one of kind udon experience you can only find at Kanbei Udon 😲",
+    url: "/images/home/specialty-3.png",
+  },
+];
+
+
+
+export default Homedata;
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,26 @@
 
 import React from 'react'
 import { Link } from "react-router-dom";
+import Homedata from './Home-data'
 
 export default function Home() {
+
+    const specialityHTML = Homedata.map((item, index) => {
+        const {title, description, url} = item
+        return (
+          <div key={index} className="specialty-card">
+            <img src={`${url}`} alt="" />
+            <div>
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+        );
+    })
+
   return (
     <main>
-      <section className="landing-page">
+      <section className="landing-page-section">
         <h1 className="shop-name">Kanbei Udon</h1>
         <p className="tagline-primary upper">
           A beloved udon shop along the streets of Kasuya, Fukuoka
@@ -32,6 +47,15 @@ export default function Home() {
         <Link to="/about" className="button-primary">
           <span>About Kanbei Udon</span>
         </Link>
+      </section>
+
+      <section className="specialty-section">
+        <h1>Our Specialty</h1>
+        {specialityHTML}
+        <h4 className='tagline-secondary'>
+          The dashi stock we use are made from scratch each morning without any
+          chemical additives.
+        </h4>
       </section>
     </main>
   );
