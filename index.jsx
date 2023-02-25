bc8f7eebcbc25d48f0047ddf3a5f9651478f09b7
Jack <li.jack0707@gmail.com>
2023-02-25 09:53:18 -0800

    chore: updated image directory

--- index.jsx
+++ index.jsx
@@ -12,25 +12,25 @@ function App() {
       <nav>
         
           <Link to="/">
-            <img src="./images/home.svg" alt="icon of a home" />
+            <img src="/images/home.svg" alt="icon of a home" />
             <span>Home</span>
           </Link>
         
         
           <Link to="/menu">
-            <img src="./images/menu.svg" alt="icon of a menu" />
+            <img src="/images/menu.svg" alt="icon of a menu" />
             <span>Menu</span>
           </Link>
         
         
           <Link to="/about">
-            <img src="./images/about.svg" alt="icon of an info icon" />
+            <img src="/images/about.svg" alt="icon of an info icon" />
             <span>About</span>
           </Link>
         
         
           <Link to="/access">
-            <img src="./images/access.svg" alt="icon of a location pin" />
+            <img src="/images/access.svg" alt="icon of a location pin" />
             <span>Access</span>
           </Link>
         
