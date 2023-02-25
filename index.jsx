eb243a6807b6cf1597f8564b255daad81f6d8a81
Jack <li.jack0707@gmail.com>
2023-02-25 10:11:16 -0800

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
