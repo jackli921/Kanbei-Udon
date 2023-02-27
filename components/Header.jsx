2afa31f17c7f4ac4691d419831c7681ac94990f4
Jack <li.jack0707@gmail.com>
2023-02-27 12:46:37 -0800

    style: added footer & positioned footer

--- components/Header.jsx
+++ components/Header.jsx
@@ -27,5 +27,6 @@ export default function Header(){
           </Link>
         </nav>
       </header>
+      
     );
 }
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,17 @@
 import React from  'react'
 import {Outlet} from "react-router-dom"
 import Header from './Header'
+import Footer from './Footer'
 
 
 export default function Layout(){
     return (
       <>
-        <Header />
-        <Outlet />
+        <div>
+          <Header />
+          <Outlet />
+        </div>
+        <Footer />
       </>
     );
 }
