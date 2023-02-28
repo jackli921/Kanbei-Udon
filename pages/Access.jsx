a82c53106973fe46ded1f3efd2ad006ea0db9c4f
Jack <li.jack0707@gmail.com>
2023-02-28 10:41:53 -0800

    feat:embedded google map on access page

--- pages/Access.jsx
+++ pages/Access.jsx
@@ -5,7 +5,17 @@ export default function Access() {
   return (
     <main className="main">
       <h1>Where is Kanbe Udon?</h1>
-      <img src="images/access/access-1.png" alt="" />
+    
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13290.462083208877!2d130.47850200000002!3d33.61527900000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3541857b0c8407f1%3A0xcf9838c6b7676b13!2sKanbei%20Udon!5e0!3m2!1sen!2sus!4v1677609279503!5m2!1sen!2sus"
+          width="90%"
+          height="300"
+          style={{ border: "0" }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      
 
       <div className="access-info-container">
         <div className="access-line-container">
