32a4eba0b670d080f862a49db2c8144b103e791c
Jack <li.jack0707@gmail.com>
2023-03-20 15:05:00 -0400

    chore: removed attributes

--- pages/About.jsx
+++ pages/About.jsx
@@ -11,10 +11,8 @@ export default function About() {
           width="100%"
           height="100%"
           src="https://www.youtube.com/embed/NvYKmCvHW7g"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          title="The Story of Kanbei Udon"
+          frameBorder="0"
         ></iframe>
       </section>
 
